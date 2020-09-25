671df5e49d7386646e352cdfbd09e956f7d4d5af
chumsrun <chumsrun@gmail.com>
2020-09-25 14:41:22 +0700

    add router

---
+++ src/components/Home/index.js
@@ -0,0 +1,469 @@
+import React from "react";
+import {Link} from "react-router-dom"
+import logo from "../../asset/logo.png";
+import api from "../../utils/api";
+import Loading from "../Layout/Loading"
+
+const mapNumber = {
+  0: "០",
+  1: "១",
+  2: "២",
+  3: "៣",
+  4: "៤",
+  5: "៥",
+  6: "៦",
+  7: "៧",
+  8: "៨",
+  9: "៩",
+};
+
+const khmerMonth = [
+  "មករា",
+  "កុម្ភៈ",
+  "មីនា",
+  "មេសា",
+  "ឧសភា",
+  "មិថុនា",
+  "កក្កដា",
+  "សីហា",
+  "កញ្ញា",
+  "តុលា",
+  "វិច្ឆិកា",
+  "ធ្នូ",
+];
+
+const khmerDay = [
+  "អាទិត្យ",
+  "ច័ន្ទ",
+  "អង្គារ",
+  "ពុធ",
+  "ព្រហស្បតិ៍",
+  "សុក្រ",
+  "សៅរ៍",
+];
+export class Meeting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.dateRef = React.createRef();
+    this.timeRef = React.createRef();
+    this.marqueeRef = React.createRef();
+    this.meetingRef = React.createRef();
+    this.token = "";
+    this.state = {
+      loading: true,
+      meeting: [],
+      date: "today",
+    };
+  }
+
+  async componentDidUpdate() {
+    clearInterval(this.intervalFetch);
+    this.intervalFetch = setInterval(async () => {
+      await this.fetchMeeting(this.state.date);
+    }, 6000);
+  }
+
+  async componentDidMount() {
+    this.dateTimeUpdate();
+    await this.fetchMeeting(this.state.date);
+    // setInterval(async () => {
+    //    await this.fetchMeeting(this.state.date);
+    // }, 3000);
+    // this.animationMeeting();
+  }
+
+  async fetchMeeting(endDate) {
+    const now = new Date();
+    if (!this.token) {
+      this.token = await api.getUserToken();
+    }
+    let endDateMeeting;
+    if (endDate === "today") {
+      endDateMeeting = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate() + 1
+      );
+    } else if (endDate === "week") {
+      const dayToWeekend = 7 - now.getDay();
+      endDateMeeting = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate() + dayToWeekend
+      );
+    } else if (endDate === "month") {
+      endDateMeeting = new Date(now.getFullYear(), now.getMonth() + 1);
+    }
+    let meeting = await api.fetchMeeting(
+      this.token,
+      new Date(endDateMeeting.getTime() + 1000 * 60 * 60 * 7).toISOString()
+    );
+    meeting = meeting?.filter(
+      (meeting) => new Date(meeting.acf.end_date_time).getTime() > Date.now()
+    );
+    if (JSON.stringify(this.state.meeting) !== JSON.stringify(meeting)) {
+      this.setState(
+        {
+          meeting: [],
+        },
+        () => {
+          clearInterval(this.intervalMeeting);
+        }
+      );
+      this.setState(
+        {
+          meeting,
+          loading: false,
+        },
+        () => {
+          //  clearInterval(this.intervalMeeting);
+          this.animationMeeting();
+        }
+      );
+    } else {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
+  animationMeeting() {
+    if (this.meetingRef.current?.children.length > 5) {
+      this.intervalMeeting = setInterval(() => {
+        let firstElement = this.meetingRef.current.firstElementChild;
+        this.meetingRef.current.children[1].classList.add("animatedDiv");
+        this.meetingRef.current.firstElementChild.remove();
+        if (firstElement.classList.contains("animatedDiv")) {
+          firstElement.className = "meeting";
+        }
+        this.meetingRef.current.insertAdjacentElement(
+          "beforeend",
+          firstElement
+        );
+      }, 12000);
+    } else {
+      let firstElement = this.meetingRef.current.firstElementChild;
+      if (firstElement) {
+        firstElement.classList.remove("animatedDiv");
+        firstElement.style.marginTop = "0px";
+      }
+    }
+  }
+
+  dateTimeUpdate() {
+    setInterval(() => {
+      var current = new Date();
+      var timeArray = current
+        .toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
+        })
+        .split(" ");
+      let date = ("0" + current.getDate())
+        .slice(-2)
+        .replace(/[0123456789]/g, (value) => {
+          return mapNumber[value];
+        });
+      let day = khmerDay[current.getDay()];
+      let month = khmerMonth[current.getMonth()];
+      let year = current
+        .getFullYear()
+        .toString()
+        .replace(/[0123456789]/g, (value) => {
+          return mapNumber[value];
+        });
+      let time = timeArray[0].replace(/[0123456789]/g, (value) => {
+        return mapNumber[value];
+      });
+
+      if (this.dateRef.current && this.timeRef.current) {
+        this.dateRef.current.textContent = `ថ្ងៃ${day} ទី${date}​ ខែ${month} ឆ្នាំ${year}`;
+        this.timeRef.current.textContent = time;
+      }
+    }, 500);
+  }
+
+  toggleFullscreen = (e) => {
+    var element = document.body;
+
+    var isFullscreen =
+      document.webkitIsFullScreen || document.mozFullScreen || false;
+
+    element.requestFullScreen =
+      element.requestFullScreen ||
+      element.webkitRequestFullScreen ||
+      element.mozRequestFullScreen ||
+      function () {
+        return false;
+      };
+    document.cancelFullScreen =
+      document.cancelFullScreen ||
+      document.webkitCancelFullScreen ||
+      document.mozCancelFullScreen ||
+      function () {
+        return false;
+      };
+
+    isFullscreen ? document.cancelFullScreen() : element.requestFullScreen();
+  };
+
+  render() {
+    const { loading, meeting } = this.state;
+    const meetingRender = meeting?.map((m, i) => {
+      let firstClass;
+      if (i === 0) firstClass = "meeting animatedDiv";
+      else firstClass = "meeting";
+
+      var start = new Date(m.date);
+      var timeArray = start
+        .toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
+        })
+        .split(" ");
+      var day = ("0" + start.getDate())
+        .slice(-2)
+        .replace(/[0123456789]/g, (value) => {
+          return mapNumber[value];
+        });
+      var month = khmerMonth[start.getMonth()];
+      var year = start
+        .getFullYear()
+        .toString()
+        .replace(/[0123456789]/g, (value) => {
+          return mapNumber[value];
+        });
+      var time =
+        timeArray[0].replace(/[0123456789]/g, (value) => {
+          return mapNumber[value];
+        }) +
+        " " +
+        (timeArray[1] === "AM" ? "ព្រឹក" : "ល្ងាច");
+
+      let mObj = (
+        <div className={firstClass} key={i}>
+          {/* Date Time */}
+          <div className="datetime">
+            <div className="date">
+              <p>
+                {day}-{month}-{year}
+              </p>
+            </div>
+            <div className="time">
+              <p>{time}</p>
+            </div>
+          </div>
+          {/* Title */}
+          <div className="title-container">
+            <div className="title">
+              <p>
+                {" "}
+                <span style={{ fontWeight: "bolder" }}>កម្មវត្ថុ៖</span>{" "}
+                {m.title.rendered}
+              </p>
+            </div>
+            <div className="info">
+              <p>
+                <span style={{ fontWeight: "bolder" }}>ដឹកនាំដោយ៖</span>{" "}
+                <span>{m.acf.leader}</span>{" "}
+                <span>
+                  {m.acf.position.length === 1
+                    ? m.acf.position
+                    : m.acf.position.join(" ")}
+                </span>
+              </p>
+            </div>
+          </div>
+
+          {/* Location */}
+          <div className="location">
+            <p>{m.acf.room}</p>
+          </div>
+          <div className="status">
+            <p className="blink">
+              {m.acf.options === "លើកពេល"
+                ? "លើកពេល"
+                : new Date(m.date).getTime() - 1000 * 60 * 30 < Date.now() &&
+                  new Date(m.date).getTime() > Date.now()
+                ? "ជិតប្រជុំ"
+                : new Date(m.date).getTime() < Date.now()
+                ? "កំពុងដំណើរការ"
+                : "រង់ចាំ"}
+            </p>
+          </div>
+        </div>
+      );
+      return mObj;
+    });
+
+    const marqueeRender = meeting?.map((m, i) => {
+      let mObj = (
+        <p key={i}>
+          {(i + 1).toString().replace(/[0123456789]/g, (value) => {
+            return mapNumber[value];
+          })}
+          / {m.title.rendered}
+        </p>
+      );
+      return mObj;
+    });
+    return (
+      <div className="App">
+        <div className="AppContainer">
+          {/* Header */}
+          <header className="App-header">
+            <div className="date-container">
+              <p className="date-header" ref={this.dateRef}>
+                ថ្ងៃអង្គារ ទី០១​ ខែកញ្ញា ឆ្នាំ២០២០
+              </p>
+              <p>
+                ម៉ោង៖{" "}
+                <span className="time-header" ref={this.timeRef}>
+                  ១១:៣៤
+                </span>{" "}
+                នាទី
+              </p>
+            </div>
+          </header>
+
+          {/* Content Body */}
+          <div className="content-container">
+            {/* Content Header */}
+            <div className="content-header">
+              <h3 className="date-caption">កាលបរិចេ្ឆទ</h3>
+              <h3 className="title-caption">ខ្លឹមសារ</h3>
+              <h3 className="location-caption">ទីតាំង</h3>
+              <h3 className="status-caption">ស្ថានភាព</h3>
+            </div>
+
+            {/* Content Body */}
+            <div className="content-body">
+              {loading && <Loading />}
+              <div className="meetingContainerSelf" ref={this.meetingRef}>
+                {!loading && meetingRender}
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Footer */}
+        <footer>
+          {/* Content Footer */}
+          <div className="content-footer">
+            <span
+              style={{ fontFamily: "Dangrek" }}
+              onClick={() => {
+                clearInterval(this.intervalMeeting);
+                this.setState(
+                  {
+                    date: "today",
+                    loading: true,
+                    meeting: null,
+                  },
+                  () => {
+                    this.fetchMeeting(this.state.date);
+                  }
+                );
+              }}
+            >
+              ថ្ងៃនេះ
+            </span>
+            <span
+              style={{ fontFamily: "Dangrek" }}
+              onClick={() => {
+                clearInterval(this.intervalMeeting);
+                this.setState(
+                  {
+                    date: "week",
+                    loading: true,
+                    meeting: null,
+                  },
+                  () => {
+                    this.fetchMeeting(this.state.date);
+                  }
+                );
+              }}
+            >
+              សប្ដាហ៌
+            </span>
+            <span
+              style={{ fontFamily: "Dangrek" }}
+              onClick={() => {
+                clearInterval(this.intervalMeeting);
+                this.setState(
+                  {
+                    loading: true,
+                    date: "month",
+                    meeting: null,
+                  },
+                  () => {
+                    this.fetchMeeting(this.state.date);
+                  }
+                );
+              }}
+            >
+              ខែនេះ
+            </span>
+
+            <div className="navigation">
+              <div className="left">
+                <i
+                  onClick={this.toggleFullscreen}
+                  className="fa fa-arrows-alt fullscreen d-lg-inline d-none"
+                  aria-hidden="true"
+                  style={{
+                    fontSize: "20px",
+                    color: "#2b6cb0",
+                    cursor: "pointer",
+                  }}
+                ></i>
+              </div>
+            </div>
+          </div>
+
+          {/* Marquee */}
+          <div className="marquee">
+            <div className="marquee-caption">
+             <Link to="allMeeting">  <img
+                src={logo}
+                alt=""
+                style={{ width: "40px", height: "40px" }}
+              /></Link>
+              <h3>
+                សរុបកិច្ចប្រជុំ
+                {this.state.date === "today"
+                  ? "ថ្ងៃ"
+                  : this.state.date === "week"
+                  ? "សប្តាហ៍"
+                  : "ខែ"}
+                នេះ មានចំនួន{" "}
+                {this.state.meeting?.length
+                  .toString()
+                  .replace(/[0123456789]/g, (value) => {
+                    return mapNumber[value];
+                  })}{" "}
+                កិច្ចប្រជុំ
+              </h3>
+            </div>
+            <marquee
+              ref={this.marqueeRef}
+              behavior="scroll"
+              direction="left"
+              onMouseOver={() => {
+                this.marqueeRef.current.stop();
+              }}
+              onMouseOut={() => {
+                this.marqueeRef.current.start();
+              }}
+            >
+              {marqueeRender}
+            </marquee>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Meeting;
---
+++ src/components/Meetings/index.js
@@ -0,0 +1,208 @@
+import React, { Component } from "react";
+import {Link} from "react-router-dom"
+import api from "../../utils/api";
+import Loading from "../Layout/Loading"
+
+
+/*
+Todo :
+  -styling
+  -render latest year 
+  -jump to current meeting
+
+*/
+
+export class MeetingAll extends Component {
+  state = {
+    meeting: [],
+    loading: true
+  };
+  componentDidMount = async () => {
+    const now = new Date();
+    const endDateMeeting = new Date(now.getFullYear(), now.getMonth() + 1);
+    const token = await api.getUserToken();
+    if (token) {
+      const meetings = await api.fetchMeeting(
+        token,
+        new Date(endDateMeeting.getTime() + 1000 * 60 * 60 * 7).toISOString()
+      );
+      this.setState({
+        meeting: meetings,
+        loading: false
+      });
+    }
+  };
+  render() {
+    const khmerMonth = [
+      "មករា",
+      "កុម្ភៈ",
+      "មីនា",
+      "មេសា",
+      "ឧសភា",
+      "មិថុនា",
+      "កក្កដា",
+      "សីហា",
+      "កញ្ញា",
+      "តុលា",
+      "វិច្ឆិកា",
+      "ធ្នូ",
+    ];
+
+    const existedMonth = [];
+    return (
+      <div className="container">
+       <Link to="/"> <h1>Home Page</h1></Link>
+        <div className="row">
+          <div className="col-md-8">
+          {this.state.loading && <Loading />}
+            {!this.state.loading&&this.state.meeting.map((v, i) => {
+              let idMonth = null;
+              let monthMeeting = new Date(v.date).getMonth()
+              if(existedMonth.indexOf(monthMeeting)<0){
+                existedMonth.push(monthMeeting);
+                idMonth = monthMeeting
+              }
+              return (
+                <div key={i} id={"month"+idMonth}>
+                  <div
+                    className="card"
+                    data-toggle="modal"
+                    data-target={"#exampleModal" + i}
+                  >
+                    <div className="card-body">
+                      <h5 className="card-title">{v.title.rendered}</h5>
+                      <p className="card-text">{new Date(v.date).toLocaleString()}</p>
+                    </div>
+                  </div>
+
+                  <div
+                    className="modal fade"
+                    id={"exampleModal" + i}
+                    tabIndex="-1"
+                    role="dialog"
+                    aria-labelledby={"exampleModalLabel" + i}
+                    aria-hidden="true"
+                  >
+                    <div className="modal-dialog" role="document">
+                      <div className="modal-content">
+                        <div className="modal-header">
+                          <h5
+                            className="modal-title"
+                            id={"exampleModalLabel" + i}
+                          >
+                            Modal title
+                          </h5>
+                          <button
+                            type="button"
+                            className="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                          >
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div className="modal-body">
+                          <ul
+                            className="nav nav-tabs"
+                            id={"myTab" + i}
+                            role="tablist"
+                          >
+                            <li className="nav-item">
+                              <a
+                                className="nav-link active"
+                                id={"home-tab" + i}
+                                data-toggle="tab"
+                                href={"#home" + i}
+                                role="tab"
+                                aria-controls="home"
+                                aria-selected="true"
+                              >
+                                Home
+                              </a>
+                            </li>
+                            <li className="nav-item">
+                              <a
+                                className="nav-link"
+                                id={"profile-tab" + i}
+                                data-toggle="tab"
+                                href={"#profile" + i}
+                                role="tab"
+                                aria-controls="profile"
+                                aria-selected="false"
+                              >
+                                Profile
+                              </a>
+                            </li>
+                            <li className="nav-item">
+                              <a
+                                className="nav-link"
+                                id={"contact-tab" + i}
+                                data-toggle="tab"
+                                href={"#contact" + i}
+                                role="tab"
+                                aria-controls="contact"
+                                aria-selected="false"
+                              >
+                                Contact
+                              </a>
+                            </li>
+                          </ul>
+                          <div className="tab-content" id="myTabContent">
+                            <div
+                              className="tab-pane fade show active"
+                              id={"home" + i}
+                              role="tabpanel"
+                              aria-labelledby="home-tab"
+                            >
+                              {v.acf.leader}
+                            </div>
+                            <div
+                              className="tab-pane fade"
+                              id={"profile" + i}
+                              role="tabpanel"
+                              aria-labelledby="profile-tab"
+                            >
+                              hi brother
+                            </div>
+                            <div
+                              className="tab-pane fade"
+                              id={"contact" + i}
+                              role="tabpanel"
+                              aria-labelledby="contact-tab"
+                            >
+                              what
+                            </div>
+                          </div>
+                        </div>
+                        <div className="modal-footer">
+                          <button
+                            type="button"
+                            className="btn btn-secondary"
+                            data-dismiss="modal"
+                          >
+                            Close
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="col-md-4">
+            {khmerMonth.map((v, i) => {
+              return (
+                <h4 key={i}>
+                  <a href={"#month" + i}>{v}</a>
+                </h4>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MeetingAll;
